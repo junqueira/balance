60853721f67f20d2500dfe108149bbb60f883911
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2014-02-27 11:55:17 -0300

    changer configure sys.argv

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'balance.db')
     MAIL_FLUSH_INTERVAL = 60  # one minute
 
 
