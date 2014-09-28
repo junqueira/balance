b99de7f7d91c1be99ee7ea04da997d48126fd08b
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2014-09-28 18:34:35 -0300

    fix model finance

--- finance/models.py
+++ finance/models.py
@@ -9,25 +9,25 @@ class TypeLaunch(models.Model):
 class Provider(models.Model):
     description = models.CharField(max_length=100, unique=True)
     type_launch = models.ForeignKey(TypeLaunch, blank=True, null=True)
-    date_last_purchase = models.DateTimeField('date last purchase')
+    date_last_purchase = models.DateField('date last purchase')
     value_total = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class Extract(models.Model):
-    date_launch = models.DateTimeField('date launch')
+    date_launch = models.DateField('date launch')
     launch = models.CharField(max_length=100)
-    date_purchase = models.DateTimeField('date purchase')
+    date_purchase = models.DateField('date purchase')
     value_debit = models.DecimalField(max_digits=5, decimal_places=2)
     value_credit = models.DecimalField(max_digits=5, decimal_places=2)
     value_balance = models.DecimalField(max_digits=5, decimal_places=2)
     cancelled = models.BooleanField(default=True, db_index=True)
     provider = models.ForeignKey(Provider, blank=True, null=True)
 
-    def str_to_date(self, date_launch, launch, year):
-        #import pdb; pdb.set_trace()
-        if launch.strip()[-3] == '/':
-            date = launch.split('-')[-1].strip()
-            date = date.replace('/','-') + '-' + str(year)
+    def str_to_date(self, date_launch, launch=''):
+        date = date_launch.replace('/','-')
+        if not launch is '' and launch.strip()[-3] == '/':
+            year = datetime.strptime(date, '%d-%m-%Y').date().year 
+            date = launch.strip()[-5:].replace('/','-') + '-' + str(year)
 
         return datetime.strptime(date, '%d-%m-%Y').date()
 
@@ -41,10 +41,9 @@ class Extract(models.Model):
             extract = Extract()
             while line <= len(contents):
                 date_launch, launch, value = contents[line].split(';')
-                extract.date_launch = datetime.strptime(date_launch, '%d-%m-%Y').date()
+                extract.date_launch = extract.str_to_date(date_launch)
                 extract.launch = launch.strip()   #.split('-')[:-1]
-                year = extract.str_to_date(date_launch).year
-                extract.date_purchase = extract.str_to_date(date_launch, launch, year)
+                extract.date_purchase = extract.str_to_date(date_launch, launch)
 
                 if extract.str_to_float(value) < 0:
                     extract.value_debit = extract.str_to_float(value)
@@ -54,6 +53,7 @@ class Extract(models.Model):
                     extract.value_credit = extract.str_to_float(value)
 
                 extract.value_balance = 0
+                import pdb; pdb.set_trace()
                 extract.save()
                 line += 1
 
