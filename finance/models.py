79f0aa4c92a707c97a2597a19fd20a4e3ac86fc4
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2014-10-04 18:34:38 -0300

    fix model finan date purchase and value is equal

--- finance/models.py
+++ finance/models.py
@@ -24,41 +24,56 @@ class Extract(models.Model):
     cancelled = models.BooleanField(default=True, db_index=True)
     provider = models.ForeignKey(Provider, blank=True, null=True)
 
-    def str_to_date(self, date_launch, launch=''):
+    def str_to_date(self, date_launch):
         date = date_launch.replace('/','-')
-        if not launch is '' and launch.strip()[-3] == '/':
-            year = datetime.strptime(date, '%d-%m-%Y').date().year 
-            date = launch.strip()[-5:].replace('/','-') + '-' + str(year)
-
         return datetime.strptime(date, '%d-%m-%Y').date()
+
+    def get_date_purchase(self, date, launch):
+        if launch.strip()[-3] == '/':
+            date_purchase = launch.strip()[-5:].replace('/','-')
+            date = date_purchase + '-' + str(date.year)
+            date = datetime.strptime(date, '%d-%m-%Y').date()
+        return date
 
     def str_to_float(self, value):
         value = float(value.replace(',','.'))
         return float("{0:.2f}".format(round(value,2)))
+
+    def is_equal(self, date_launch, launch, value):
+        if value < 0:
+            t = Extract.objects.filter(date_launch=date_launch, launch=launch.strip(), value_debit=abs(value)) 
+        else:
+            t = Extract.objects.filter(date_launch=date_launch, launch=launch.strip(), value_credit=value) 
+
+        return t
 
     def importer(self, path='/Users/neto/Dropbox/projetcs/balance-pack/extrato.txt'):
         with open(path, 'r') as ff:
             contents = ff.readlines()
             line = 0
             while line < len(contents):
-                #import pdb; pdb.set_trace()
-                date_launch, launch, value = contents[line].split(';')
-
                 extract = Extract()
-                extract.date_launch = extract.str_to_date(date_launch)
-                extract.launch = launch.strip()   #.split('-')[:-1]
-                extract.date_purchase = extract.str_to_date(date_launch, launch)
+                date_launch, launch, value = contents[line].split(';')
+                date_launch = extract.str_to_date(date_launch)
+                launch = launch.strip()   #.split('-')[:-1]
+                value = extract.str_to_float(value)
                 
-                if extract.str_to_float(value) < 0:
-                    extract.value_debit = abs(extract.str_to_float(value))
-                    extract.value_credit = 0
-                else:
-                    extract.value_debit = 0
-                    extract.value_credit = extract.str_to_float(value)
+                #import pdb; pdb.set_trace()
+                if not extract.is_equal(date_launch, launch, value).exists():    
+                    extract.date_launch = date_launch
+                    extract.launch = launch
+                    extract.date_purchase = extract.get_date_purchase(date_launch, launch)
+                    
+                    if value < 0:
+                        extract.value_debit = abs(value)
+                        extract.value_credit = 0
+                    else:
+                        extract.value_debit = 0
+                        extract.value_credit = value
 
-                extract.value_balance = 0
-                
-                extract.save()
+                    extract.value_balance = 0
+                    extract.save()
+
                 line += 1
 
             ff.close()
