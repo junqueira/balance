91547e620f9b0dcd8a8e8d0f75d33981fc4f1af6
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2014-10-04 23:07:28 -0300

    inclusion cust to finance provider with launch date

--- finance/models.py
+++ finance/models.py
@@ -13,6 +13,12 @@ class Provider(models.Model):
     date_last_purchase = models.DateField('date last purchase')
     value_total = models.DecimalField(max_digits=8, decimal_places=2)
 
+    def update(self, launch):
+        provider = Provider.objects.get(description=launch)
+        provider.date_last_purchase = self.date_last_purchase
+        provider.value_total = self.value_total
+        provider.save()
+
 
 class Extract(models.Model):
     date_launch = models.DateField('date launch')
@@ -29,8 +35,8 @@ class Extract(models.Model):
         return datetime.strptime(date, '%d-%m-%Y').date()
 
     def get_date_purchase(self, date, launch):
-        if launch.strip()[-3] == '/':
-            date_purchase = launch.strip()[-5:].replace('/','-')
+        if launch[-3] == '/':
+            date_purchase = launch[-5:].replace('/','-')
             date = date_purchase + '-' + str(date.year)
             date = datetime.strptime(date, '%d-%m-%Y').date()
         return date
@@ -41,11 +47,37 @@ class Extract(models.Model):
 
     def is_equal(self, date_launch, launch, value):
         if value < 0:
-            t = Extract.objects.filter(date_launch=date_launch, launch=launch.strip(), value_debit=abs(value)) 
+            t = Extract.objects.filter(date_launch=date_launch, launch=launch, value_debit=abs(value))
         else:
-            t = Extract.objects.filter(date_launch=date_launch, launch=launch.strip(), value_credit=value) 
-
+            t = Extract.objects.filter(date_launch=date_launch, launch=launch, value_credit=value)
         return t
+
+    def get_launch(self, launch):
+        launch = launch.strip()
+        if launch[-3] == '/':
+            launch = launch[:-6].strip()
+        return launch
+
+    def get_value_total(self, launch):
+        total = 0
+        for value in Extract.objects.filter(launch=launch):
+            if value.value_debit > 0:
+                total += value.value_debit
+            else:
+                total += value.value_credit
+        return total
+
+    def provider(self, launch):
+        #import pdb; pdb.set_trace()
+        provider = Provider()
+        provider.description = launch
+        provider.date_last_purchase = Extract.objects.filter(launch=launch).last().date_purchase
+        provider.value_total = self.get_value_total(launch)
+
+        if not Provider.objects.filter(description=launch).exists():
+            provider.save()
+        else:
+            provider.update(launch)
 
     def importer(self, path='/Users/neto/Dropbox/projetcs/balance-pack/extrato.txt'):
         with open(path, 'r') as ff:
@@ -55,15 +87,14 @@ class Extract(models.Model):
                 extract = Extract()
                 date_launch, launch, value = contents[line].split(';')
                 date_launch = extract.str_to_date(date_launch)
-                launch = launch.strip()   #.split('-')[:-1]
+                launch = extract.get_launch(launch)
                 value = extract.str_to_float(value)
-                
-                #import pdb; pdb.set_trace()
-                if not extract.is_equal(date_launch, launch, value).exists():    
+
+                if not extract.is_equal(date_launch, launch, value).exists():
                     extract.date_launch = date_launch
                     extract.launch = launch
                     extract.date_purchase = extract.get_date_purchase(date_launch, launch)
-                    
+
                     if value < 0:
                         extract.value_debit = abs(value)
                         extract.value_credit = 0
@@ -74,6 +105,7 @@ class Extract(models.Model):
                     extract.value_balance = 0
                     extract.save()
 
+                extract.provider(launch)
                 line += 1
 
             ff.close()
