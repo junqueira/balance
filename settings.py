d79bf8615d2d145c29726941fe9683f3326a72e8
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2014-11-02 17:20:52 -0200

    fix config base heroku

--- settings.py
+++ settings.py
@@ -79,6 +79,15 @@ WSGI_APPLICATION = 'wsgi.application'
 #         }
 #     }
 
+#Storage on S3 settings are stored as os.environs to keep settings.py clean
+# if not DEBUG:
+#    AWS_STORAGE_BUCKET_NAME =  'secure-mountain-6385'   #os.environ['AWS_STORAGE_BUCKET_NAME']
+#    AWS_ACCESS_KEY_ID =    os.environ['AWS_ACCESS_KEY_ID']
+#    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#    STATIC_URL = S3_URL
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
